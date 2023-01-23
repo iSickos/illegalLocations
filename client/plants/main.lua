f252619f2e7e17178817bbb169a34363882c1b49
iSicko <74508163+iSickos@users.noreply.github.com>
2023-01-23 02:12:44 +0200

    Prettify

--- client/plants/main.lua
+++ client/plants/main.lua
@@ -1,9 +1,7 @@
 spawnedProps = {}
 spawnedPropsCount = {}
 
-
 for i, zone in pairs(Config.plantZones) do
-    
     Citizen.CreateThread(function()
         if zone.blip then
             createBlip(zone.coords, zone.blipName, zone.density, zone.spriteColor, zone.sprite, zone.circleColor)
@@ -19,7 +17,8 @@ for i, zone in pairs(Config.plantZones) do
             local playerCoords = GetEntityCoords(PlayerPedId())
             local targetCoords = vector3(zone.coords)
             local distance = #(playerCoords - targetCoords)
-            if (distance < zone.density * 2) then
+
+            if distance < zone.density * 2 then
                 spawnPlants(zone, i)
             else
                 Wait(500)
@@ -28,13 +27,9 @@ for i, zone in pairs(Config.plantZones) do
     end)
 end
 
-
-
 function spawnPlants(zone, i)
-
-    while (spawnedPropsCount[i] < zone.maxPropCount) do
-
-        if (zone.growTime) then
+    while spawnedPropsCount[i] < zone.maxPropCount do
+        if zone.growTime then
             Wait(zone.growTime * 1000)
         else
             Wait(0)
@@ -52,28 +47,29 @@ function spawnPlants(zone, i)
                 {
                     icon = zone.icon,
                     label = zone.harvestText,
-                    canInteract = function() return not IsPedInAnyVehicle(PlayerPedId(), false) end,
+                    canInteract = function()
+                        return not IsPedInAnyVehicle(PlayerPedId(), false)
+                    end,
                     onSelect = function(data)
                         local nearbyId
-                        for j = 1, #spawnedProps[i], 1 do
+
+                        for j = 1, #spawnedProps[i] do
                             if data.entity == spawnedProps[i][j] then
                                 nearbyId = j
                             end
                         end
+
                         local targetPropId = data.entity
-                        itemCheck(zone, targetPropId, nearbyId, i)
+                        runCheck(zone, targetPropId, nearbyId, i)
                     end
                 }
             }
-    
-            exports.ox_target:addLocalEntity(obj, options)
 
+            exports.ox_target:addLocalEntity(obj, options)
         end)
 
-        spawnedPropsCount[i] += 1
-
+        spawnedPropsCount[i] = 1
     end
-
 end
 
 function generatePlantCoords(zone)
@@ -87,7 +83,6 @@ function generatePlantCoords(zone)
 
         math.randomseed(GetGameTimer())
         local modY = math.random(-zone.density, zone.density)
-
 
         local plantCoordX = zone.coords.x + modX
         local plantCoordY = zone.coords.y + modY
@@ -114,36 +109,27 @@ function generatePlantCoords(zone)
             zone.coords.z + 7,
             zone.coords.z + 8,
             zone.coords.z + 9,
-            zone.coords.z + 10
-        }
+            zone.coords.z + 10}
 
         local ground, plantCoordZ
+
         for i, height in ipairs(groundCheckHeights) do
             ground, plantCoordZ = GetGroundZFor_3dCoord(plantCoordX, plantCoordY, height)
-
-            if ground then
-                break;
-            end
-
+            if ground then break end
         end
 
         local generatedCoords = vector3(plantCoordX, plantCoordY, plantCoordZ)
 
         return generatedCoords
-
     end
-
 end
 
 AddEventHandler('onResourceStop', function(resource)
-
     if resource == GetCurrentResourceName() then
-
         for i, zone in pairs(Config.plantZones) do
             for j, v in pairs(spawnedProps[i]) do
                 ESX.Game.DeleteObject(v)
             end
         end
     end
-
 end)
