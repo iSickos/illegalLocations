4b8ea878825764fef8bb37ff53b4cd855de61b10
iSicko <74508163+iSickos@users.noreply.github.com>
2023-02-20 12:47:41 +0200

    refactor(server/main.lua): Simplification

--- server/main.lua
+++ server/main.lua
@@ -1,30 +1,23 @@
-ESX = exports['es_extended']:getSharedObject()
+if Config.Framework == 'esx' then
+    ESX = exports['es_extended']:getSharedObject()
+end
 
-lib.callback.register('esx_illegal:canCarryItem', function(source, item, amount)
-    
-    if exports.ox_inventory:CanCarryItem(source, item, amount) then
-        return true
-    else
-        return false
-    end
-
+lib.callback.register('illegalLocations:removeItem', function(source, obj)
+    return exports.ox_inventory:RemoveItem(source, obj.takeItem, obj.takeAmount)
 end)
 
-lib.callback.register('esx_illegal:canSwapItem', function(source, itemOne, itemOneAmount, itemTwo, itemTwoAmount)
-    
-    if exports.ox_inventory:CanSwapItem(source, itemOne, itemOneAmount, itemTwo, itemTwoAmount) then
-        return true
-    else
-        return false
-    end
-    
+lib.callback.register('illegalLocations:canCarryItem', function(source, item, amount)
+    return exports.ox_inventory:CanCarryItem(source, item, amount)
 end)
 
-lib.callback.register('esx_illegal:hasAllItems', function(source, item)
+lib.callback.register('illegalLocations:canSwapItem', function(source, itemOne, itemOneAmount, itemTwo, itemTwoAmount)
+    return exports.ox_inventory:CanSwapItem(source, itemOne, itemOneAmount, itemTwo, itemTwoAmount)
+end)
 
+lib.callback.register('illegalLocations:hasAllItems', function(source, itemList)
     local allExist = true
 
-    for itemName, requiredCount in pairs(item) do
+    for itemName, requiredCount in pairs(itemList) do
         local count = exports.ox_inventory:GetItem(source, itemName, nil, true)
         if count < requiredCount then
             allExist = false
@@ -32,18 +25,11 @@ lib.callback.register('esx_illegal:hasAllItems', function(source, item)
         end
     end
 
-    if allExist then
-        return true
-    else
-        return false
-    end
-
+    return allExist
 end)
 
-RegisterServerEvent("esx_illegal:giveItem")
-AddEventHandler("esx_illegal:giveItem", function(obj)
-
-    local playerCoords  = GetEntityCoords(GetPlayerPed(source))
+lib.callback.register('illegalLocations:giveItem', function(source, obj)
+    local playerCoords = GetEntityCoords(GetPlayerPed(source))
     local targetCoords
     local maxDistance
 
@@ -70,39 +56,19 @@ AddEventHandler("esx_illegal:giveItem", function(obj)
         print('ID: '.. source .. ' may have cheats.')
         print('Tried to get weed at Weed Field')
         print('His distance from Weed Field is: ' .. distance)
-
         --add your log system
     end
-
+    return success
 end)
 
-lib.callback.register('esx_illegal:takeItem', function(source, obj)
-
-    local success = exports.ox_inventory:RemoveItem(source, obj.takeItem, obj.takeAmount)
-
-    return(success)
-
-end)
-
-lib.callback.register('esx_illegal:countJob', function(source, jobsObj)
-    local xPlayers = ESX.GetPlayers()
-    local jobCount = {}
-    
-    for k, v in pairs(xPlayers) do
-        local xPlayer = ESX.GetPlayerFromId(v)
-        local playerJob = xPlayer.job.name
-    
-        if jobsObj[playerJob] then
-            if not jobCount[playerJob] then
-                jobCount[playerJob] = 0
-            end
-            jobCount[playerJob] += 1
+lib.callback.register('illegalLocations:countJob', function(source, jobsObj)
+    if Config.Framework == 'esx' then
+        for jobName, reqCount in pairs(jobsObj) do
+            if #ESX.GetExtendedPlayers("job", jobName) < reqCount then return false end
         end
-    end
-    
-    for jobName, reqCount in pairs(jobsObj) do
-        if (not jobCount[jobName]) or jobCount[jobName] < reqCount then
-            return false
+    elseif Config.Framework == 'ox' then
+        for jobName, reqCount in pairs(jobsObj) do
+            if #Ox.GetPlayerByFilter({ group = jobName }) < reqCount then return false end
         end
     end
     
